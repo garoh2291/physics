35ff954f9a6163d5e99b7c35e73c18761bcf6014
garoh2291 <garoh2291@gmail.com>
2025-08-28 19:24:58 +0400

    fix image issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "prisma"],
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push("@prisma/client");
